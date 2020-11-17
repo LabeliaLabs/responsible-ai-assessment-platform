b065d5854311c3f63afd187a98ac84ec9c23cb73
natct10 <nathanael.cretin@substra.org>
2020-11-17 16:33:58 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+docker-compose -f docker-compose.prod.yml down && \
+	git fetch && \
+	git pull --rebase && \
+	docker-compose -f docker-compose.prod.yml up --build -d && \
+	docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations && \
+	docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput && \
+	docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+
